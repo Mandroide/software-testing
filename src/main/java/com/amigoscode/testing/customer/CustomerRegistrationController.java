c847cb7a09bf2be64af9f6cad1f63315f99e3d05
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2022-07-09 12:45:58 -0400

    Fix endpoint of CustomerRegistrationController

--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("api/v1/customer-registartion")
+@RequestMapping("api/v1/customer-registration")
 public class CustomerRegistrationController {
 
     private final CustomerRegistrationService service;
